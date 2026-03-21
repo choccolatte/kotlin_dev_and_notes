941e75301b6c9a07affc66c5da711721c1b9a8a2
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-03-22 00:25:32 +0530

    prac run - v2203260025

--- Prac.java
+++ Prac.java
@@ -2,12 +2,34 @@ import java.time.Year;
 
 public class Prac {
 	// define a variable
-	int currYear = 2026;
-	// check if variable equals condition
-	if (currYear == Year.now){
-		System.out.println("Year: " + currYear);
-		return 0;
+	// int currYear = 2026;
+
+	// // check if variable equals condition
+	// if (currYear == Year.now){
+	// 	System.out.println("Year: " + currYear);
+	// 	// return 0;
+	// }
+	// System.out.println("Year: " + Year.now());
+	// return 1;
+
+	// lowest and highest in an int array
+	int[] nums = {45, 12, 98, 33, 27, 9, 100, 23, 34, 45, 56, 65, 76, 87, 98, 11};
+
+	int minx = nums[0];
+	int maxx = nums[0];
+
+	for(int num : nums){
+		// highest
+		if(mmaxx < num){
+			maxx = num;
+		}
+
+		// lowest
+		if(num < minx){
+			minx = num;
+		}
 	}
-	System.out.println("Year: " + Year.now());
-	return 1;
+
+	System.out.println("Highest number: " + maxx);
+	System.out.println("Lowest number: " + minx);
 }
