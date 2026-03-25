ae85e092bed88f104c31ffe28373bb5c16104451
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-03-25 23:43:08 +0530

    a class practise - v2503262343

--- Prac.java
+++ Prac.java
@@ -1,62 +1,32 @@
-// import java.time.Year;
-
 public class Prac {
-	// define a variable
-	// int currYear = 2026;
+	int modelYear;
+	String modelName;
 
-	// // check if variable equals condition
-	// if (currYear == Year.now){
-	// 	System.out.println("Year: " + currYear);
-	// 	// return 0;
-	// }
-	// System.out.println("Year: " + Year.now());
-	// return 1;
-
-	// lowest and highest in an int array
-	// int[] nums = {45, 12, 98, 33, 27, 9, 100, 23, 34, 45, 56, 65, 76, 87, 98, 11};
-
-	// int minx = nums[0];
-	// int maxx = nums[0];
-
-	// for(int num : nums){
-	// 	// highest
-	// 	if(mmaxx < num){
-	// 		maxx = num;
-	// 	}
-
-	// 	// lowest
-	// 	if(num < minx){
-	// 		minx = num;
-	// 	}
-	// }
-
-	// System.out.println("Highest number: " + maxx);
-	// System.out.println("Lowest number: " + minx);
-
-	// recursion
-	// public static int sum(int k){
-	// 	if (k > 0){
-	// 		return k + sum(k -1);
-	// 	} else {
-	// 		return 0;
-	// 	}
-	// }
-
-	// public static void main(String[] args){
-	// 	int res = sum(10);
-	// 	System.out.println(res);
-	// }
-
-	// factorial using recursion
-	static int factorial(int n){
-		if(n > 1){
-			return n * factorial(n-1);
-		} else{
-			return 1;
-		}
+	// co=nstructor with one parameter
+	public Prac(String modelName){
+		this(2025, modelName);
 	}
 
-	static void main(String[] args){
-		System.out.println(factorial(10));
+	// co=nstructor with two parameters
+	public Prac(int modelYear, String modelName){
+		this.modelName = modelName;
+		this.modelYear = modelYear;
 	}
+
+	// merthod to print car info
+	public void printInfo(){
+		System.out.println(modelYear + " " + modelName);
+	}
+
+	// main func
+	public static void main(String[] args){
+		// car with one para
+		Prac myCar = new Prac("Mustang");
+
+		// car with 2 paras
+		Prac myCar2 = new Prac(2026, "Mustang");
+
+		myCar.printInfo();
+		myCar2.printInfo();
+	}	
 }
