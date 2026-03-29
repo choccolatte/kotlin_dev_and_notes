6452a3e44f05c850fe28a840a1a0330ec45e6788
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-03-29 23:59:37 +0530

    implement interfaces - v2903262359

--- ScanE.java
+++ ScanE.java
@@ -1,11 +1,22 @@
-import java.util.Scanner;
+// interfaces
+interface ScaneAni {
+	public void animalSound();
+	public void sleep();
+}
 
-public class ScanE {
+// implementing animal interface
+public class ScanE implements ScaneAni{
+	public void animalSound(){
+		System.out.println("Merrrw");
+	}
+
+	public void sleep(){
+		System.out.println("Zzzz");
+	}
+}
+
+class Main{
 	public static void main(String[] args){
-		Scanner myObj = new Scanner(System.in);
-		System.out.println("Enter username: ");
-
-		String usrName = myObj.nextLine();
-		System.out.println("User: " + usrName);
+		
 	}
 }
