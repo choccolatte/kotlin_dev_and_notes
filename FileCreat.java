f49acaee68ff4f0353943305221992bc28bee557
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-04-03 00:01:17 +0530

    appended to text file - v0304260001

--- FileCreat.java
+++ FileCreat.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 public class FileCreat {
 	public static void main(String[] args){
 		// using try-with-resources
-		try(FileWriter myFil = new FileWriter("newFil.txt")){
-			myFil.write("This is a dummy text file with dummy text values.");
+		try(FileWriter myFil = new FileWriter("newFil.txt", true)){
+			myFil.append("\nNNow, this is the new appended text.");
 			System.out.println("Successfully wrote to file.");
 		}catch(IOException e){
 			System.out.println("Error occured while writing to file.");
