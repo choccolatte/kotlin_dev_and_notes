97d9e6312e994d46f85eb488f389471713b89cb3
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-04-04 23:59:35 +0530

    moving on to DSA in java - v0404262359

---
+++ IterAl.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IterAl {
+	public static void main(String[] args){
+		// create an arraylist of strings
+		ArrayList<String> alist = new ArrayList<String>();
+		alist.add("BMW");
+		alist.add("Volvo");
+		alist.add("Honda");
+		alist.add("Toyota");
+		alist.add("Ford");
+
+		// get an iterator for the ArrayList
+		Iterator<String> iter = alist.iterator();
+
+		// iterate through the list using the iterator
+	}
+	
+}
