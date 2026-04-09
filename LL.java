6d5aca6f7166a0e58e3af8010a0df45d8639039b
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-04-10 02:33:14 +0530

    working with LL - v1004260233

---
+++ LL.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class LL {
+	public static void main(String[] args){
+		var cars = new LinkedList<String>();
+		cars.add("Honda");
+		cars.add("Toyota");
+		cars.add("BMW");
+
+		System.out.println(cars);
+	}
+}
