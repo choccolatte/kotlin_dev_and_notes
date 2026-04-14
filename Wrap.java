99f2ea7c2fc5b47af6099f7f9d9497d00cb39e6d
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-04-14 23:42:32 +0530

    wrapper classes and objects - v1404262342

---
+++ Wrap.java
@@ -0,0 +1,12 @@
+public class Wrap {
+	public static void main(String[] args){
+		Integer myInt = 50;
+		Double myDou = 50.55;
+		Character myChar = 'a';
+
+		System.out.println(myInt);
+		System.out.println(myDou);
+		System.out.println(myChar);
+
+	}
+}
